0331c05a0d7fd5c15fb7df9d14679ecaa46de407
Clem JOURDIN <cjourdin@enssat.fr>
2026-03-20 00:39:35 +0000

    Player prefab updated after last modifs to Player from GameplayScene

--- unity/Assets/Scripts/PlayerCollision.cs
+++ unity/Assets/Scripts/PlayerCollision.cs
@@ -12,14 +12,9 @@ public class PlayerCollision : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         if (!collision.gameObject.CompareTag("obstacle")) return;
-
-        // On boucle sur tous les points de contact de la collision
         foreach (ContactPoint contact in collision.contacts)
         {
             Vector3 normal = contact.normal;
-
-            // Vérifier si la collision vient d'un côté (±X ou ±Z)
-            // La composante Y normale sera proche de 0 si c'est un côté
             if (Mathf.Abs(normal.y) < 0.5f)
             {
                 if (player != null)
@@ -32,12 +27,7 @@ public class PlayerCollision : MonoBehaviour
                     Debug.LogError("Player component not found!");
                 }
 
-                break; // On ne traite qu'un point de contact valide
-            }
-            else
-            {
-                // Collision par le dessus ou dessous : ignorée
-                Debug.Log("Collision ignorée car venant de haut ou bas");
+                break;
             }
         }
     }
