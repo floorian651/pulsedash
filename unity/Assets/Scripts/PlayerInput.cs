41522ce3b93b10543b8971405e840faebd2c7f87
Arthur Blamart <arthurblamart@gmail.com>
2026-03-20 10:43:59 +0100

    Trois lignes de deplacements fonctionnelles

--- unity/Assets/Scripts/PlayerInput.cs
+++ unity/Assets/Scripts/PlayerInput.cs
@@ -7,12 +7,11 @@ public class PlayerMovementE5 : MonoBehaviour
     Rigidbody rb;
 
     Vector3 jump;
-    float moveRight = 2f;
-    float moveUp = 30f;
+    public float distanceEntreLigne = 2.0f;
+    public enum PositionX{Gauche,Milieu,Droite}
+    PositionX currentSidepos = PositionX.Milieu;
+    float moveUp = 20f;
     float forwardSpeed = 3f;
-    float maxSpeed = 5f;
-    float currentLeftSpeed;
-    float currentRightSpeed;
 
     bool isGrounded;
     void Start()
@@ -29,25 +28,34 @@ public class PlayerMovementE5 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.forward * Time.deltaTime * forwardSpeed;
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             // Jumping
             rb.AddForce(jump * moveUp, ForceMode.Impulse);
             isGrounded = false;
         }
+
+        float sideMovement = 0;
+
+        // Si on appuye sur Q
+        if (Input.GetKeyDown(KeyCode.A) && currentSidepos!=PositionX.Gauche)
+        {
+            sideMovement = -distanceEntreLigne;
+            currentSidepos--;
+        }
+
+        // Si on appuye sur D
+        if (Input.GetKeyDown(KeyCode.D) && currentSidepos!=PositionX.Droite)
+        {
+            sideMovement = distanceEntreLigne;
+            currentSidepos++;
+        }
+
+        Vector3 side = new Vector3(sideMovement, 0, 0);
+
+        // On applique tout les vecteurs à la position
+        transform.position += Vector3.forward * Time.deltaTime * forwardSpeed + side;
     }
-
-    void FixedUpdate()
-    {
-        float moveX = Input.GetAxis("Horizontal") * moveRight;
-
-        Vector3 velocity = rb.linearVelocity;
-        velocity.x = moveX;
-
-        rb.linearVelocity = Vector3.ClampMagnitude(velocity, maxSpeed);
-    }
-
     // Getters and Setters de vitesse (vroom)
     public float GetSpeed()
     {
