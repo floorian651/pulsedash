8a631275d3b6f2e3eb61376cb8a24f1859a5f947
Clem JOURDIN <cjourdin@enssat.fr>
2026-04-02 19:17:01 +0000

    Obstacles disappearing after collision + damage taken

--- unity/Assets/Scripts/Player/PlayerCollision.cs
+++ unity/Assets/Scripts/Player/PlayerCollision.cs
@@ -11,7 +11,7 @@ public class PlayerCollision : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.LogError("Collision avec " + collision.gameObject.tag);
+        //Debug.LogError("Collision avec " + collision.gameObject.tag);
         if (!collision.gameObject.CompareTag("obstacle")) return;
         Debug.LogError("Collision avec un obstacle");
         foreach (ContactPoint contact in collision.contacts)
