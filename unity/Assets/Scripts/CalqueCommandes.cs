7ee8daa6197c36102dff8b75774492cb6385ed78
vicou <vrouet@enssat.fr>
2026-04-13 18:07:19 +0200

    new:Ajout IU indications commandes fix

--- unity/Assets/Scripts/CalqueCommandes.cs
+++ unity/Assets/Scripts/CalqueCommandes.cs
@@ -9,12 +9,40 @@ public class CalqueCommandes : MonoBehaviour
     public float fadeDuration    = 1f; // Durée du fondu de disparition
 
     private Image _image;
+    private RectTransform _rectTransform;
+
+    [Header("Taille")]
+    public float screenRatio = 0.9f; // 9/10 de l'écran
+    public float gapRatio = 0.1f;
+    
 
     void Start()
     {
-        _image = GetComponent<Image>();
+        _image = GetComponent<Image>(); 
+
+        _rectTransform = GetComponent<RectTransform>();
+        
+        AdjustSize();
         _image.enabled = true;
         StartCoroutine(HideAfterDelay());
+    }
+
+    void AdjustSize()
+    {
+        // Récupère la taille de l'écran
+        float screenWidth  = Screen.width;
+        float screenHeight = Screen.height;
+
+        // Calcule 9/10 de la taille de l'écran
+        float targetWidth  = screenWidth  * screenRatio;
+        float targetHeight = screenHeight * screenRatio;
+
+        float targetGap  = screenHeight  * gapRatio;
+        
+
+        // Applique la taille au RectTransform
+        _rectTransform.sizeDelta = new Vector2(targetWidth, targetHeight);
+        _rectTransform.anchoredPosition = new Vector2(0f,targetGap); 
     }
 
     IEnumerator HideAfterDelay()
